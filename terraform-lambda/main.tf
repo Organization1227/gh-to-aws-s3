a4b98fe61c7a5647bba8cf86e832a46f84cccac7
kanampalli <himaja.kanampalli@gmail.com>
2024-03-28 02:53:13 -0400

    Update main.tf

--- terraform-lambda/main.tf
+++ terraform-lambda/main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_openid_connect_provider" "github_actions_oidc_provider" {
-  url            = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["${lookup(data.external.get_thumbprint.result, "thumbprint")}"]
-}
-
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
   assume_role_policy = jsonencode({
