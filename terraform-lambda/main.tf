a0de4edc3b6fc69fb05ef8493a7a43c66704dd3e
kanampalli <himaja.kanampalli@gmail.com>
2024-03-28 02:57:23 -0400

    Update main.tf

--- terraform-lambda/main.tf
+++ terraform-lambda/main.tf
@@ -2,19 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_openid_connect_provider" "github_actions_oidc_provider" {
+  # Specify the ARN of your identity provider here
+  arn = "arn:aws:iam::338435713321:oidc-provider/token.actions.githubusercontent.com"
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action    = "sts:AssumeRole"
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github_actions_oidc_provider.arn
+      },
+      Action    = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:sub" = "system:serviceaccount:namespace:serviceaccount"
+        }
       }
-    ]
+    }]
   })
 }
 
