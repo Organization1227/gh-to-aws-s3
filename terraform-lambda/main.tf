8b377d2eec411aa9faa8fd5095e1bb35cc242099
kanampalli <himaja.kanampalli@gmail.com>
2024-03-28 03:09:54 -0400

    Update main.tf

--- terraform-lambda/main.tf
+++ terraform-lambda/main.tf
@@ -2,31 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_openid_connect_provider" "github_actions_oidc_provider" {
-  # Specify the ARN of your identity provider here
-  arn = "arn:aws:iam::338435713321:oidc-provider/token.actions.githubusercontent.com"
+resource "aws_lambda_function" "hello_world" {
+  # Set the necessary Lambda function configurations
+  # For example:
+  filename      = "index.js"
+  function_name = "hello-world-function"
+  role          = "arn:aws:iam::338435713321:role/lambda_role" # Replace with the actual ARN of your existing IAM role
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github_actions_oidc_provider.arn
-      },
-      Action    = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = {
-          "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:sub" = "system:serviceaccount:namespace:serviceaccount"
-        }
-      }
-    }]
-  })
-}
-
-output "lambda_role_arn" {
-  value = aws_iam_role.lambda_role.arn
+output "lambda_function_arn" {
+  value = aws_lambda_function.hello_world.arn
 }
