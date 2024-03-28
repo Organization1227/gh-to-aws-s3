e8cfd6a5e057ba1a841f8319aadaf6cc860b35f7
kanampalli <himaja.kanampalli@gmail.com>
2024-03-28 01:44:47 -0400

    Create main.tf

---
+++ terraform-lambda/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "hello_world" {
+  filename      = "lambda.zip"
+  function_name = "hello-world-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Federated = "https://token.actions.githubusercontent.com"
+        },
+        Action    = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
